e0d0b8f38a41863b172be72c26aa87d5cdb0ec4a
Yubin QIU <Yubin.Qiu@kp.org>
2019-03-06 09:28:18 +0000

    yubin:update

--- adidas.py
+++ adidas.py
@@ -10,7 +10,7 @@ from pyvirtualdisplay import Display
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 from selenium.webdriver.support.ui import WebDriverWait
-
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 url="https://www.adidas.com/us/men-basketball-shoes"

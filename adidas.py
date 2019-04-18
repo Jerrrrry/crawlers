a68bb8a9b427555d2b48ec472210b67ea24a1525
Yubin QIU <Yubin.Qiu@kp.org>
2019-04-18 21:00:45 +0000

    yubin:update

--- adidas.py
+++ adidas.py
@@ -16,8 +16,9 @@ from selenium.webdriver.support import expected_conditions as EC
 url="https://www.gasbuddy.com/GasPrices/California/Riverside"
 options = ChromeOptions()
 options.add_argument("--headless")
-#options.add_argument('--no-sandbox')
+options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.setExperimentalOption('useAutomationExtension', false)
 #options.setBinary("/var/chromedriver/chromedriver")
 chromeDriverPath = "/var/chromedriver/chromedriver"
 driver = webdriver.Chrome(chromeDriverPath,chrome_options=options)
